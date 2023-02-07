855b67a469f85e5ab8fb0a28a3ea79d073d95c18
shtlamrut <109715486+shtlamrut@users.noreply.github.com>
2023-02-07 11:05:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
-app.get('/ready', function (req, res) {
-    res.send('{ "response": "It works!" }');
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
-app.listen(process.env.PORT || 3000);
-module.exports = app;
